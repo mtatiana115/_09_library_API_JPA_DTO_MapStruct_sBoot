2ab978672983e7575b8c0e2d5125b9eeb3e7ce64
mtatiana115 <mtatiana115@gmail.com>
2024-06-21 15:33:09 -0500

    feature/user entity complete with path

--- src/main/java/com/riwi/library/api/controllers/GenericController.java
+++ src/main/java/com/riwi/library/api/controllers/GenericController.java
@@ -2,14 +2,14 @@ package com.riwi.library.api.controllers;
 
 import org.springframework.http.ResponseEntity;
 
-public interface GenericController <RequestDTO, ResponseDTO> {
+public interface GenericController <RequestDTO, ResponseDTO, ID> {
 
   public ResponseEntity<ResponseDTO> create(RequestDTO request);
 
-  public ResponseEntity<ResponseDTO> get(Long id);
+  public ResponseEntity<ResponseDTO> get(ID id);
 
-  public ResponseEntity<ResponseDTO> update (RequestDTO request, Long id);
+  public ResponseEntity<ResponseDTO> update (RequestDTO request, ID id);
   
-  public ResponseEntity<Void> delete(Long id); 
+  public ResponseEntity<Void> delete(ID id); 
   
 }
--- src/main/java/com/riwi/library/api/controllers/UserController.java
+++ src/main/java/com/riwi/library/api/controllers/UserController.java
@@ -1,5 +1,48 @@
 package com.riwi.library.api.controllers;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.riwi.library.api.dto.request.UserRequest;
+import com.riwi.library.api.dto.response.UserResponse;
+import com.riwi.library.infrastructure.abstract_services.IUserService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/users")
+public class UserController implements GenericController <UserRequest, UserResponse, Long> {
+
+  @Autowired
+  IUserService userService;
+
+  @PostMapping
+  public ResponseEntity<UserResponse> create(@RequestBody UserRequest request) {
+    return ResponseEntity.ok(this.userService.create(request));
+  }
+
+  @Override
+  public ResponseEntity<UserResponse> get(Long id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'get'");
+  }
+
+  @Override
+  public ResponseEntity<UserResponse> update(UserRequest request, Long id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'update'");
+  }
+
+  @Override
+  public ResponseEntity<Void> delete(Long id) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'delete'");
+  }
+
 
 }
